4ee7d1a809b0c2de9c9b682820b65f94c3b6843a
appledora <immobile.imbecile0010@gmail.com>
2019-02-15 01:44:12 +0600

    stuff

--- resources/views/comingsoon.blade.php
+++ resources/views/comingsoon.blade.php
@@ -5,20 +5,20 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	<link rel="icon" type="image/png" href="{{asset("images/icons/favicon.ico")}}"/>
 <!--/-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href={{asset("vendor/bootstrap/css/bootstrap.min.css")}}>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="{{asset("fonts/font-awesome-4.7.0/css/font-awesome.min.css")}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" type="text/css" href="{{asset("fonts/iconic/css/material-design-iconic-font.min.css")}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="{{asset("vendor/animate/animate.css")}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="{{asset("vendor/select2/select2.min.css")}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="{{asset("css/util.css")}}">
+	<link rel="stylesheet" type="text/css" href="{{asset("css/main.css")}}">
 <!--===============================================================================================-->
 </head>
 <body>
@@ -115,17 +115,17 @@
 	
 
 <!--===============================================================================================-->	
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="{{asset("vendor/jquery/jquery-3.2.1.min.js")}}"></script>
 <!--===============================================================================================-->
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="{{asset("vendor/bootstrap/js/popper.js")}}"></script>
+	<script src="{{asset("vendor/bootstrap/js/bootstrap.min.js")}}"></script>
 <!--===============================================================================================-->
-	<script src="vendor/select2/select2.min.js"></script>
+	<script src="{{asset("vendor/select2/select2.min.js")}}"></script>
 <!--===============================================================================================-->
-	<script src="vendor/countdowntime/moment.min.js"></script>
-	<script src="vendor/countdowntime/moment-timezone.min.js"></script>
-	<script src="vendor/countdowntime/moment-timezone-with-data.min.js"></script>
-	<script src="vendor/countdowntime/countdowntime.js"></script>
+	<script src="{{asset("vendor/countdowntime/moment.min.js")}}"></script>
+	<script src="{{asset("vendor/countdowntime/moment-timezone.min.js")}}"></script>
+	<script src="{{asset("vendor/countdowntime/moment-timezone-with-data.min.js") }}"></script>
+	<script src="{{asset("vendor/countdowntime/countdowntime.js") }}"></script>
 	<script>
 		$('.cd100').countdown100({
 			/*Set Endtime here*/
